bf0e7b2ad27a48d370bd95887ec1ae7e86dbb334
AnilNembang <anilnembang99@gmail.com>
2020-01-10 22:05:14 -0500

    Revert "added third line in main method"
    This reverts commit 22b59567f4d43577f1693ed4324303ff455a70b7.

--- zzzz/src/com/project/CommitTest.java
+++ zzzz/src/com/project/CommitTest.java
@@ -4,6 +4,5 @@ public class CommitTest {
 	public static void main(String[] args) {
 		System.out.println("test to commit");
 		System.out.println("helo");
-		System.out.println("testing commit and push");
 	}
 }
