22b59567f4d43577f1693ed4324303ff455a70b7
AnilNembang <anilnembang99@gmail.com>
2020-01-10 21:57:49 -0500

    added third line in main method

--- zzzz/src/com/project/CommitTest.java
+++ zzzz/src/com/project/CommitTest.java
@@ -4,5 +4,6 @@ public class CommitTest {
 	public static void main(String[] args) {
 		System.out.println("test to commit");
 		System.out.println("helo");
+		System.out.println("testing commit and push");
 	}
 }
